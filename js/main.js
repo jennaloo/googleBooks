54e62be74a0f40b2ed719380c05a938ef7341524
Jenna Murphy <jenna.raye.murphy@gmail.com>
2018-11-21 14:56:52 -0800

    added book info

--- js/main.js
+++ js/main.js
@@ -15,11 +15,18 @@ function getBook() {
 
                 var resultContainer = document.createElement('div');
                 var newImg = document.createElement('img');
+                var newH3 = document.createElement('h3');
+                var newP = document.createElement('p');
+
+                resultContainer.className = "col-12 text-center";
 
                 newImg.src = bookData.imageLinks.thumbnail;
-
+                newH3.innerText = bookData.title;
+                newP.innerText = bookData.description;
                 document.getElementById('results').appendChild(resultContainer);
                 resultContainer.appendChild(newImg);
+                resultContainer.appendChild(newH3);
+                resultContainer.appendChild(newP);
             }
         },
 
