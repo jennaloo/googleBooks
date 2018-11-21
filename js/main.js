16bc1b7a9040f5d14bd331d4842ca1ffd03f7da4
Jenna Murphy <jenna.raye.murphy@gmail.com>
2018-11-21 14:37:58 -0800

    clears on click

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 function getBook() {
 
+    var results = document.getElementById("results").innerHTML = "";
+
     var book = document.getElementById('bookSearch').value;
+
     $.ajax({
         url: 'https://www.googleapis.com/books/v1/volumes?q=' + book + '',
         dataType: "json",
         success: function (data) {
             console.log(data);
+
             for (i = 0; i < data.items.length; i++) {
+                var bookData = data.items[i].volumeInfo;
 
-                var image = data.items[i].volumeInfo.imageLinks.thumbnail;
+                var resultContainer = document.createElement('div');
+                var newImg = document.createElement('img');
 
-                document.getElementById('book').innerHTML += '<img src=' + image + '>';
+                newImg.src = bookData.imageLinks.thumbnail;
 
-                var title = data.items[i].volumeInfo.title;
-                document.getElementById('book').innerHTML += '<h3>' + title + '</h3>';
-
-                var info = data.items[i].volumeInfo.description;
-                if (info !== undefined) {
-                    document.getElementById('book').innerHTML += '<p>' + info + '</p>';
-                }
-
+                document.getElementById('results').appendChild(resultContainer);
+                resultContainer.appendChild(newImg);
             }
         },
 
